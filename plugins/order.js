f0a37ce26794c2d7f77821a3bd79ab4bfcaffe41
Matt DuLeone <matt@duleone.com>
2017-10-13 16:23:37 -0400

    fixes

--- plugins/order.js
+++ plugins/order.js
@@ -1,7 +1,11 @@
 const pizzapi = require('dominos')
 
 function setUpConvo(err, convo) {
-  convo.addMessage(`Woof woof! Hello <@${message.user}>! This is the best day ever! I am finding nearby pizza places for you! Squirrel!\r\nYour options are\r\n ${response}`, 'thread_1')
+  console.log(convo);
+  convo.addMessage({
+    text: `Woof woof! Hello <@${convo.context.user}>! This is the best day ever! Where do you want pizza? I will find nearby pizza places for you! Squirrel!`,
+    action: 'thread_2'
+  }, 'thread_1')
   convo.addQuestion('What is your address?', (responseObj, conversation) => {
     convo.setVar('address', responseObj.text)
     pizzapi.Util.findNearbyStores(convo.vars.address, 'Delivery', (storeData) => {
@@ -13,7 +17,7 @@ function setUpConvo(err, convo) {
           convo.gotoThread('thread_3')
       })
   }, {}, 'thread_2')
-  convo.addMessage(`Here you go! These are the nearby stores \r\n ${convo.vars.response}`, 'thread_3')
+  convo.addMessage(`Here you go! These are the nearby stores \r\n {{ vars.response }}`, 'thread_3')
   
   convo.activate()
   convo.gotoThread('thread_1')
@@ -22,7 +26,7 @@ function setUpConvo(err, convo) {
 
 module.exports = {
   init: (controller) => {
-    controller.hears([/I want a pizza/], ['direct_message', 'direct_mention'], (bot, message) => bot.createConversation(message, setUpConvo))
+    controller.hears([/I want a pizza/], ['direct_message', 'ambient'], (bot, message) => bot.createConversation(message, setUpConvo))
   },
   help: {
     command: 'welcome',
